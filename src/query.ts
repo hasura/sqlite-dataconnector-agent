cfddab4481bff2ccbc34a452d2447317487baa62
Lyndon Maydwell <lyndon@sordina.net>
2022-07-07 12:21:32 +1000

    WIP outer json_group_array

--- src/query.ts
+++ src/query.ts
@@ -1,6 +1,6 @@
 ﻿import { Config }  from "./config";
 import { connect } from "./db";
-import { omap }    from "./util";
+import { coerceUndefinedToNull, omap }    from "./util";
 import {
     Expression,
     BinaryComparisonOperator,
@@ -15,24 +15,11 @@ import {
     Relationship,
     RelationshipField,
     RelationshipType,
-    Field,
     BinaryArrayComparisonOperator,
     Fields, 
   } from "./types/query";
 
-// function array_relationship_object(rs: Array<TableRelationships>, k: string, v: Field): string {
-//   console.log("array_relationship_object", k, v);
-
-//   switch(v.type) {
-//     case "column":
-//       return `'${k}', ${v.column}`;
-//     case "relationship":
-//       console.log("Sub-field relationships not supported yet", k, v, v.query.fields);
-//       return `'${k}', ${fields2(rs, v.query.field)}`;
-//   }
-// }
-
-function array_relationship_object(rs: Array<TableRelationships>, fs: Fields, t: string): string {
+function relationship_object(escapeSQL: EscapeSQL, rs: Array<TableRelationships>, fs: Fields, t: string): string {
   return omap(fs, (k,v) => {
     switch(v.type) {
       case "column":
@@ -42,7 +29,7 @@ function array_relationship_object(rs: Array<TableRelationships>, fs: Fields, t:
           if(x.source_table === t) {
             const rel = x.relationships[v.relationship];
             if(rel) {
-              return [`'${k}', ${relationship(rs, rel, v, t)}`];
+              return [`'${k}', ${relationship(escapeSQL, rs, rel, v, t)}`];
             }
           }
           console.log("Couldn't find relationship for field", k, v, rs);
@@ -52,25 +39,25 @@ function array_relationship_object(rs: Array<TableRelationships>, fs: Fields, t:
   }).flatMap((e) => e).join(", ");
 }
 
-function array_relationship_where(w: Expression | null | undefined): Array<string> {
+function relationship_where(w: Expression | null): Array<string> {
   if(w == null) {
     return [];
   } else {
     switch(w.type) {
       case "not":
-        const aNot = array_relationship_where(w.expression);
+        const aNot = relationship_where(w.expression);
         if(aNot.length > 0) {
           return [`(NOT ${aNot})`];
         }
         break;
       case "and":
-        const aAnd = w.expressions.flatMap(array_relationship_where);
+        const aAnd = w.expressions.flatMap(relationship_where);
         if(aAnd.length > 0) {
           return [`(${aAnd.join(" AND ")})`];
         }
         break;
       case "or":
-        const aOr = w.expressions.flatMap(array_relationship_where);
+        const aOr = w.expressions.flatMap(relationship_where);
         if(aOr.length > 0) {
           return [`(${aOr.join(" OR ")})`];
         }
@@ -92,7 +79,31 @@ function array_relationship_where(w: Expression | null | undefined): Array<strin
   }
 }
 
-function relationship(rs: Array<TableRelationships>, r: Relationship, f: RelationshipField, t: string): string {
+// TODO: Use wWhere?: instead of | null | undefined
+function array_relationship(
+    escapeSQL: EscapeSQL,
+    ts: Array<TableRelationships>,
+    wTable: string,
+    wJoin: Array<string>,
+    wFields: Fields,
+    wWhere: Expression | null,
+    wLimit: number | null,
+    wOffset: number | null,
+  ): string {
+      const wFilter = relationship_where(wWhere);
+      // TODO: Ensure that the table prefixes are correct - currently assuming it's from "parent" to "child"
+      return `(
+        SELECT json_group_array(j)
+        FROM (
+          SELECT json_object(${relationship_object(escapeSQL, ts, wFields, wTable)}) as j
+          FROM ${wTable}
+          ${whereN(escapeSQL, wWhere, wJoin)}
+          ${limit(wLimit)}
+          ${offset(wOffset)}
+        ))`;
+}
+
+function relationship(escapeSQL: EscapeSQL, ts: Array<TableRelationships>, r: Relationship, f: RelationshipField, t: string): string {
   // (select json_group_array(json_object('Title', Album.Title)) from Album where Album.ArtistId = Artist.ArtistId) as J
 
   console.log("relationship", f, r);
@@ -106,22 +117,26 @@ function relationship(rs: Array<TableRelationships>, r: Relationship, f: Relatio
 
     case RelationshipType.Array:
       const wJoin   = omap(r.column_mapping, (k,v) => `${t}.${k} = ${r.target_table}.${v}`);
-      const wFilter = array_relationship_where(f.query.where);
-      // TODO: Ensure that the table prefixes are correct - currently assuming it's from "parent" to "child"
-      return `
-        (select json_group_array(json_object(${array_relationship_object(rs, f.query.fields, r.target_table)}))
-          from ${r.target_table}
-          where ${[...wJoin, ...wFilter].join(" AND ")})
-      `;
+      // const wFilter = relationship_where(f.query.where);
+      return array_relationship(
+        escapeSQL,
+        ts,
+        r.target_table,
+        wJoin,
+        f.query.fields,
+        coerceUndefinedToNull(f.query.where),
+        coerceUndefinedToNull(f.query.limit),
+        coerceUndefinedToNull(f.query.offset),
+        );
       // return `
-      //   (select json_group_array(json_object(${omap(f.query.fields, (k,v) => array_relationship_object(rs,k,v)).join(", ")}))
+      //   (select json_group_array(json_object(${relationship_object(ts, f.query.fields, r.target_table)}))
       //     from ${r.target_table}
       //     where ${[...wJoin, ...wFilter].join(" AND ")})
       // `;
   }
 }
 
-function fields(rs: Array<TableRelationships>, r: QueryRequest): string {
+function fields(escapeSQL: EscapeSQL, rs: Array<TableRelationships>, r: QueryRequest): string {
   return omap(r.query.fields, (k,v) => {
     switch(v.type) {
       case "column":
@@ -131,7 +146,7 @@ function fields(rs: Array<TableRelationships>, r: QueryRequest): string {
           if(x.source_table === r.table) {
             const rel = x.relationships[v.relationship];
             if(rel) {
-              return [`${relationship(rs, rel, v, r.table)} as ${k}`];
+              return [`${relationship(escapeSQL, rs, rel, v, r.table)} as ${k}`];
             }
           }
           console.log("Couldn't find relationship for field", k, v, rs);
@@ -234,43 +249,73 @@ function where(escapeSQL: EscapeSQL, w:Expression): string {
   }
 }
 
-function whereN(escapeSQL: EscapeSQL, w: Expression | null | undefined): string {
+function whereN(escapeSQL: EscapeSQL, w: Expression | null, j: Array<string>,): string {
   if(w == null) {
     return "";
   } else {
-    const r = where(escapeSQL, w);
-    if(r === "") {
+    const r = relationship_where(w);
+    if(r.length < 1) {
       return "";
     } else {
-      return `where ${r}`;
+      return `WHERE ${[...r, ...j].join(" AND ")}`;
     }
   }
 }
 
-function limit(r: QueryRequest): string {
-  if(r.query.limit == null) {
+function limit(l: number | null): string {
+  if(l === null) {
     return "";
   } else {
-    return `limit ${r.query.limit}`;
+    return `LIMIT ${l}`;
   }
 }
 
-function offset(r: QueryRequest): string {
-  if(r.query.offset == null) {
+function offset(o: number | null): string {
+  if(o == null) {
     return "";
   } else {
-    return `offset ${r.query.offset}`;
+    return `OFFSET ${o}`;
   }
 }
 
 type EscapeSQL = (s: string) => string
 
-function query(t: Array<TableRelationships>, escapeSQL: EscapeSQL, r: QueryRequest): string {
-  return `select ${fields(t, r)} from ${escapeSQL(r.table)} ${whereN(escapeSQL, r.query.where)} ${limit(r)} ${offset(r)}`;
+function object_relationship(escapeSQL: EscapeSQL, ts: Array<TableRelationships>, q: QueryRequest ): string {
+  const wFilter = relationship_where(coerceUndefinedToNull(q.query.where));
+  return `
+    (select json_object(${relationship_object(escapeSQL, q.table_relationships, q.query.fields, q.table)})
+      FROM ${q.table}
+      WHERE ${wFilter.join(" AND ")})
+  `;
 }
 
-function output(rs: any): Array<ProjectedRow> {
-  return rs;
+// function query(t: Array<TableRelationships>, escapeSQL: EscapeSQL, q: QueryRequest): string {
+//   // return `select ${fields(t, r)} from ${escapeSQL(r.table)} ${whereN(escapeSQL, r.query.where)} ${limit(r)} ${offset(r)}`;
+//   return `
+//     SELECT ${object_relationship(escapeSQL, t, q)}
+//     FROM ${escapeSQL(q.table)}
+//     ${whereN(escapeSQL, q.query.where)}
+//     ${limit(q)}
+//     ${offset(q)}`;
+// }
+
+function query(t: Array<TableRelationships>, escapeSQL: EscapeSQL, r: QueryRequest): string {
+  // return `select ${fields(t, r)} from ${escapeSQL(r.table)} ${whereN(escapeSQL, r.query.where)} ${limit(r)} ${offset(r)}`;
+  const q = array_relationship(
+    escapeSQL,
+    r.table_relationships,
+    r.table,
+    [],
+    r.query.fields,
+    coerceUndefinedToNull(r.query.where),
+    coerceUndefinedToNull(r.query.limit),
+    coerceUndefinedToNull(r.query.offset),
+    );
+  return `SELECT ${q} as data`;
+}
+
+function output(r: any): Array<ProjectedRow> {
+  return JSON.parse(r[0].data); // TODO: What to do if there are no results? (Should be impossible.)
 }
 
 /** Performs a query and returns results
@@ -292,27 +337,40 @@ function output(rs: any): Array<ProjectedRow> {
  * 
  * ```
  * query MyQuery {
- *   Artist(limit: 5, order_by: {ArtistId: asc}, where: {Name: {_neq: "Accept"}, _and: {Name: {_is_null: false}}}) {
- *     ArtistId
- *     Name
- *     Albums(where: {Title: {_is_null: false, _gt: "A", _nin: "foo"}}) {
- *       AlbumId
- *       Title
+ *   Artist(limit: 5, order_by: {ArtistId: asc, Albums_aggregate: {}}, where: {Name: {_neq: "Accept"}, _and: {Name: {_is_null: false}}}) {
  *       ArtistId
- *       Tracks {
- *         Name
- *         TrackId
+ *       Name
+ *       Albums(where: {Title: {_is_null: false, _gt: "A", _nin: "foo"}}, limit: 2) {
+ *         AlbumId
+ *         Title
+ *         ArtistId
+ *         Tracks(limit: 1) {
+ *           Name
+ *           TrackId
+ *         }
  *       }
  *     }
  *   }
- * }
  * ```
+ * 
+ * Example of Raw Queries:
+ * 
+ * sqlite> select json_group_array(j) from (select json_object('who', name) as j from artist limit 5);
+ *   [{"who":"AC/DC"},{"who":"Accept"},{"who":"Aerosmith"},{"who":"Alanis Morissette"},{"who":"Alice In Chains"}]
+ * 
+ * sqlite> select json_group_array(j) from (select json_object('who', name) as j from artist where name not like '%Aero%' limit 5 offset 1);
+ *   [{"who":"Accept"},{"who":"Alanis Morissette"},{"who":"Alice In Chains"},{"who":"Antônio Carlos Jobim"},{"who":"Apocalyptica"}]
+ * 
+ * sqlite> select json_group_array(j) from (select json_object('who', name, 'album', (select json_group_array(k) from (select json_object('t', Title) as k from Album where Album.artistId = Artist.artistId))) as j from artist where name not like '%Aero%' limit 5 offset 1);
+ *   [{"who":"Accept","album":[{"t":"Balls to the Wall"},{"t":"Restless and Wild"}]},{"who":"Alanis Morissette","album":[{"t":"Jagged Little Pill"}]},{"who":"Alice In Chains","album":[{"t":"Facelift"}]},{"who":"Antônio Carlos Jobim","album":[{"t":"Warner 25 Anos"},{"t":"Chill: Brazil (Disc 2)"}]},{"who":"Apocalyptica","album":[{"t":"Plays Metallica By Four Cellos"}]}]
  */
 export async function queryData(config: Config, queryRequest: QueryRequest): Promise<Array<ProjectedRow>> {
   console.log(queryRequest);
   const db     = connect(config);             // TODO: Should this be cached?
   const esc    = (s: string) => db.escape(s); // TODO: Thread escaper to other functions
+  // TODO: Could the depth of recursion be a problem?
   const q      = query(queryRequest.table_relationships, esc, queryRequest);    // TODO: Could the depth of recursion be a problem?
+  // const q      = array_relationship(queryRequest.table_relationships, queryRequest.table, [], queryRequest.query.fields, queryRequest.query.where);
   const [r, m] = await db.query(q);
   const o      = output(r);
   return o;
